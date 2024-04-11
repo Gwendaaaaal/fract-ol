be1c50c4fa46bc28197cb8530fe24d8a164ce797
Gwendal Hollocou <gholloco@paul-f3Br4s8.clusters.42paris.fr>
2024-04-11 17:01:03 +0200

    changes to init.c guards

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: gholloco <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 15:49:50 by gholloco          #+#    #+#             */
-/*   Updated: 2024/04/11 16:43:55 by gholloco         ###   ########.fr       */
+/*   Updated: 2024/04/11 16:56:22 by gholloco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ int	init_mlx(t_data *data)
 		return (0);
 	data->win = mlx_new_window(data->mlx, WIDTH, HEIGHT, "fract-ol");
 	if (!data->win)
-		return (free(data->win), 0);
+		return (free(data->mlx), 0);
 	data->img.img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
 	if (!data->img.img)
-		return (0);
+		return (mlx_destroy_window(data->mlx, data->win), free(data->mlx), 0);
 	data->img.addr = mlx_get_data_addr(data->img.img,
 			&(data->img.bpp), &(data->img.line_len), &(data->img.endian));
 	if (!data->img.addr)
-		return (0);
+		return (free_then_exit(data), 0);
 	mlx_hook(data->win, DestroyNotify, 0, &handle_x, data);
 	mlx_key_hook(data->win, &handle_key_event, data);
 	mlx_mouse_hook(data->win, &handle_mouse_event, data);
